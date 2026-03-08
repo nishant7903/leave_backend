2191f05b931baf56e68d0614a4627c8f06c170e1
nishant7903 <nishantranjan7903@gmail.com>
2026-03-08 13:46:23 +0530

    updated mongo connect

--- db_config/mongo_connect.js
+++ db_config/mongo_connect.js
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose';
 
+
 const connectDB = async () => {
     try {
-        const uri = `${process.env.MONGO_URI}/${process.env.MONGO_DB}`;
-        const conn = await mongoose.connect(uri);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`);
-        process.exit(1); // Exit process with failure
+        // Appending authSource=admin as it is required for authentication on some cloud providers
+        const uri = `${process.env.MONGO_URI}/${process.env.MONGO_DB}?authSource=admin`;
+        const sanitizedUri = uri.replace(/:([^:@]+)@/, ":****@");
+        await mongoose.connect(uri);
+        console.log("Connected to mongo db");
+    } catch (e) {
+        console.error("Error while connecting to mongo server: ", e);
+        process.exit(1);
     }
 };
-
 export default connectDB;
